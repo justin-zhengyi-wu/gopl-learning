4349a62727a3ec4e0e646270900c7f3559ae1566
Justin Wu <justin.zhengyi.wu@gmail.com>
2017-04-10 22:56:11 +0800

    complete chapter 1

--- ch1/fetch2.go
+++ ch1/fetch2.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
 
+/**
+Exercise 1.7
+*/
 func main() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(url)
@@ -14,13 +17,12 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		_, err2 := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		if err2 != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err2)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 
 }
